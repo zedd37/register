5f3935c71a61220d17f27d2bd8be724a848e457f
zedd37 <ziadsamer37@gmail.com>
2022-11-27 23:30:18 +0200

    new updates on login form

--- src/app/sign-in-form/sign-in-form.component.ts
+++ src/app/sign-in-form/sign-in-form.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in-form',
   templateUrl: './sign-in-form.component.html',
   styleUrls: ['./sign-in-form.component.css']
 })
-export class SignInFormComponent {
+export class SignInFormComponent implements OnInit {
+  constructor() {
 
+  }
+  ngOnInit(): void {
+
+  }
+
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    pwd: new FormControl("", [Validators.required, Validators.minLength(6), Validators.maxLength(15)])
+  });
+
+
+  loginSubmitted() {
+    console.log(this.loginForm);
+  }
+
+
+
+  get Email(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get PWD(): FormControl {
+    return this.loginForm.get('pwd') as FormControl;
+  }
 }
